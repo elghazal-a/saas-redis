cc9ef5337e87d76f9ac91bdcc90d43d9005d3842
geniousphp <geniousphp@gmail.com>
2015-03-30 19:24:00 +0100

    base app

---
+++ client/app/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('nvd3App', [
+  'ngResource',
+  'ngRoute'
+])
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+
+    $locationProvider.html5Mode(true);
+  });
---
+++ client/components/navbar/navbar.controller.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('nvd3App')
+  .controller('NavbarCtrl', function ($scope, $location) {
+    $scope.menu = [{
+      'title': 'Home',
+      'link': '/'
+    }];
+
+    $scope.isCollapsed = true;
+
+    $scope.isActive = function(route) {
+      return route === $location.path();
+    };
+  });
