649e8e2da7887b12c948ceead288f375a0e1b085
geniousphp <geniousphp@gmail.com>
2015-03-31 01:44:12 +0100

    spawn

--- server/api/instance/instance.controller.js
+++ server/api/instance/instance.controller.js
@@ -7,25 +7,30 @@ function spawnRedisInstance(name){
   var docker_path = '/usr/bin/';
   var cmd_runRedis = docker_path + 'docker run -d -P --name="' + name + '" memcached';
   var container_id = exec(cmd_runRedis);
-  console.log(container_id);
   var cmd_inspect = docker_path + 'docker inspect ' + container_id;
   var json_inspect = exec(cmd_inspect);
-  console.log(json_inspect);
   try{
     json_inspect = JSON.parse(json_inspect);
-    console.log(json_inspect[0].NetworkSettings);
   }catch(err){
-    throw err;
+    return port = null;
   }
   var port = json_inspect[0].NetworkSettings.Ports["11211/tcp"][0].HostPort;
   return port;
 };
 
 exports.new = function(req, res) {
+  var ip = "176.31.119.123";
+  var port = spawnRedisInstance(req.body.name);
+  res.json({
+    ip: ip,
+    port: port
+  });
+};
+
+exports.list = function(req, res) {
   console.log(req.body.name);
   var ip = "176.31.119.123";
   var port = spawnRedisInstance(req.body.name);
-  console.log(port);
   res.json({
     ip: ip,
     port: port
